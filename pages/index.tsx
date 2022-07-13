d70f9a1b8e158a7b300479b13f3b1d42fe1a1778
Stephanie Sanchez <stephk006@gmail.com>
2022-07-13 16:43:31 -0400

    created frame for page components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,16 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div>Hello empty project</div>;
+  return (
+    <div>
+      <div>Stephanie Sanchez</div>
+      <div>Software Engineer</div>
+      <div>Welcome!</div>
+      <div>Some Text</div>
+      <div>Project 1</div>
+      <div>Designed and built Stephanie</div>
+    </div>
+  );
 };
 
 export default Home;
