c1ec3ddd0fc960daa3b750c3a603886818cb426c
Stephanie Sanchez <stephk006@gmail.com>
2022-07-14 21:55:22 -0400

    created profile component

---
+++ components/profile.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <div>
+      <div>Welcome!</div>
+      <div>Some Text</div>
+    </div>
+  );
+};
+
+export default Profile;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Footer from "../components/footer";
+import Header from "../components/header";
+import Profile from "../components/profile";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <div>Stephanie Sanchez</div>
-      <div>Software Engineer</div>
-      <div>Welcome!</div>
-      <div>Some Text</div>
+      <Header />
+      <Profile />
       <div>Project 1</div>
       <Footer />
     </div>
