8d2ca0feebb850f2396a552aba2147d48d9df9c2
Stephanie Sanchez <stephk006@gmail.com>
2022-07-14 22:28:53 -0400

    created layout component to try to make good on big screen as well as mobile

---
+++ components/layout.tsx
@@ -0,0 +1,5 @@
+const Layout = (props: any) => {
+  return <div className="max-w-sm">{props.children}</div>;
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,22 @@ import Head from "next/head";
 import Image from "next/image";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import Layout from "../components/layout";
 import Profile from "../components/profile";
 import ProjectPreview from "../components/project-preview";
 
 const Home: NextPage = () => {
   return (
-    <div className="text-slate-700">
-      <Header />
-      <Profile />
-      <div>
-        <ProjectPreview />
+    <Layout>
+      <div className="text-slate-700">
+        <Header />
+        <Profile />
+        <div>
+          <ProjectPreview />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Layout>
   );
 };
 
