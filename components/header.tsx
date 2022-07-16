f21e073aacfed73aa79b304e6139d280014a8a19
Stephanie Sanchez <stephk006@gmail.com>
2022-07-15 22:42:48 -0400

    cleaned up repition && moved styling to top level

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
-    <div className="text-center border-b border-slate-300 p-4">
-      <div className="text-2xl font-bold">Stephanie Sanchez</div>
-      <div className="text-gray-500">Software Engineer</div>
-      <div className="flex">
-        <div className="text-gray-500 text-sm">Email: your@email.com</div>
-        <div className="text-gray-500 text-sm">Phone: 305-555-1234</div>
+    <div className="text-center border-b border-slate-300 p-4 text-gray-500">
+      <div className="text-2xl font-bold text-slate-700">Stephanie Sanchez</div>
+      <div>Software Engineer</div>
+      <div className="text-sm flex justify-center gap-2">
+        <div>Email: your@email.com</div>
+        <div>Phone: 305-555-1234</div>
       </div>
     </div>
   );
