6239b0a89905f7d271055388cf6ef313159a9587
Stephanie Sanchez <stephk006@gmail.com>
2022-07-15 22:36:27 -0400

    added contact info

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,10 @@ const Header = () => {
     <div className="text-center border-b border-slate-300 p-4">
       <div className="text-2xl font-bold">Stephanie Sanchez</div>
       <div className="text-gray-500">Software Engineer</div>
+      <div className="flex">
+        <div className="text-gray-500 text-sm">Email: your@email.com</div>
+        <div className="text-gray-500 text-sm">Phone: 305-555-1234</div>
+      </div>
     </div>
   );
 };
