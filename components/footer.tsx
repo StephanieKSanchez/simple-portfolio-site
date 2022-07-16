644ba61efce51062f47930f9c625511174bfd5f0
Stephanie Sanchez <stephk006@gmail.com>
2022-07-15 22:28:07 -0400

    increased gap

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="text-center border-t border-slate-300 p-4r">
+    <div className="text-center border-t border-slate-300 p-4">
       <div>Designed and built Stephanie</div>
     </div>
   );
