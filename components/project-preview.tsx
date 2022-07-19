8e60b67afe06cccc035ed369012b4232f543908f
Stephanie Sanchez <stephk006@gmail.com>
2022-07-18 21:51:59 -0400

    imported Image from NEXT and added pic

--- components/project-preview.tsx
+++ components/project-preview.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 const ProjectPreview = () => {
   const title: string = "Data Analysis App";
   const desc: string = "Processes data in Python";
 
   return (
     <div>
+      <Image src={"/coding.jpg"} />
       <a href={"/"} className="font-bold underline hover:text-indigo-600">
         {title}
       </a>
