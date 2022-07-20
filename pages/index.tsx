91de049cdc5855309ea404c869e136a1a6f523e6
Stephanie Sanchez <stephk006@gmail.com>
2022-07-20 17:29:40 -0400

    cleaned up this file too

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Footer from "../components/footer";
-import Header from "../components/header";
-import Layout from "../components/layout";
 import Profile from "../components/profile";
 import ProjectPreview from "../components/project-preview";
 
