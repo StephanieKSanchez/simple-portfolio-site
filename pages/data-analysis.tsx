2632dd59f8c0053992971018266006999be9f515
Stephanie Sanchez <stephk006@gmail.com>
2022-07-20 15:41:43 -0400

    added back

--- pages/data-analysis.tsx
+++ pages/data-analysis.tsx
@@ -4,9 +4,19 @@ import Image from "next/image";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Layout from "../components/layout";
+import Link from "next/link";
 
 const DataAnalysis: NextPage = () => {
-  return <div>Something about my data analysis app here...</div>;
+  return (
+    <div>
+      <div>Something about my data analysis app here...</div>
+      <div className="mt-6">
+        <Link href="/">
+          <a className="font-bold underline hover:text-indigo-600">Back</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default DataAnalysis;
