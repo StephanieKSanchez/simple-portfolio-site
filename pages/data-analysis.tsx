e6047e36395c8d788a49ddcb2bafa2d2f74b3292
Stephanie Sanchez <stephk006@gmail.com>
2022-07-20 17:27:32 -0400

    cleaned up project deleting unused code in this file

--- pages/data-analysis.tsx
+++ pages/data-analysis.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
-import Footer from "../components/footer";
-import Header from "../components/header";
-import Layout from "../components/layout";
 import Link from "next/link";
 import projectImage from "../public/coding.jpg";
 
