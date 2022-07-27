265e0144cd4f1fbad8ecf4e4de4294256862e05c
Stephanie Sanchez <stephk006@gmail.com>
2022-07-26 22:34:59 -0400

    copied code from Font Awesome to import icon

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
+  const element = <FontAwesomeIcon icon={faCoffee} />;
   return (
     <div className="text-center border-t border-slate-300 p-4 text-slate-400">
       <div>Designed and built Stephanie</div>
