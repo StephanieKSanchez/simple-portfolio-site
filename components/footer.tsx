42d5fde6b2ceb0cc4c24d2280667a5453564f9ee
Stephanie Sanchez <stephk006@gmail.com>
2022-07-26 22:38:18 -0400

    icon renders now

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,10 +2,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
-  const element = <FontAwesomeIcon icon={faCoffee} />;
+  const icon = <FontAwesomeIcon icon={faCoffee} />;
   return (
     <div className="text-center border-t border-slate-300 p-4 text-slate-400">
       <div>Designed and built Stephanie</div>
+      {icon}
     </div>
   );
 };
