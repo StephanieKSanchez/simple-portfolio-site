29f8a0f88a21cbf32579a9161d305e7df841ae93
Stephanie Sanchez <stephk006@gmail.com>
2022-07-27 13:06:12 -0400

    made icon clickable

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
 
 const Footer = () => {
   const icon = <FontAwesomeIcon icon={faGithub} />;
   return (
     <div className="text-center border-t border-slate-300 p-4 text-slate-400">
       <div>Designed and built Stephanie</div>
-      <div className="text-3xl text-slate-300">{icon}</div>
+      <Link href="https://github.com/StephanieKSanchez">
+        <a className="text-3xl text-slate-300">{icon}</a>
+      </Link>
     </div>
   );
 };
