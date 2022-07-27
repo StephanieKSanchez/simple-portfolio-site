b13d06c76c3d871c4dff9e8dde74c4f129b9cc84
Stephanie Sanchez <stephk006@gmail.com>
2022-07-26 22:44:40 -0400

    faded icon a bit

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="text-center border-t border-slate-300 p-4 text-slate-400">
       <div>Designed and built Stephanie</div>
-      <div className="text-3xl">{icon}</div>
+      <div className="text-3xl text-slate-300">{icon}</div>
     </div>
   );
 };
